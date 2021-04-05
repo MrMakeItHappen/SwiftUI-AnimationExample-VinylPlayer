440535f888bfad47f60ce3628ff0032de6eda4b0
Stanley Miller <smiller8203@gmail.com>
2021-04-05 12:07:54 -0400

    Initial Commit

---
+++ Practice-Animations02/Practice_Animations02App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Animations02App.swift
+//  Practice-Animations02
+//
+//  Created by Stanley Miller on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Animations02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
